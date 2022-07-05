95d00a5d3e5e6434687ab6927699b3ef7e627414
Dawson Hanson <Dawson.Hanson2@gmail.com>
2022-07-04 20:27:15 -0600

    Edit javascript.js File
    Added simple age checker using prompts.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,8 +1,19 @@
-let name = 'John';
+let name = prompt ('What is your name?');
 let admin;
 admin = name;
 
-alert(name)
-alert(admin)
+alert(`Hello ${admin}, nice to meet you!`)
+alert(`${admin}, I think thats an awesome name`)
 
 console.log("hello world")
+
+const age = prompt ('How old are you?');
+let myNum = Number(age)
+if (age >= 14 && age <= 90) {
+  alert ('Enjoy your stay!')
+} else  {
+  alert ('Sorry! You\'re either to young or to old to be here!.')
+}
+
+console.log (typeof myNum)
+console.log (typeof age)
