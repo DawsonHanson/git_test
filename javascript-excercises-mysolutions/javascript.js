fdf29d6bbe3bbb457684af79270d9131459c98e9
Dawson Hanson <Dawson.Hanson2@gmail.com>
2022-08-22 18:10:03 -0600

    Edit javascript.js File
    Added Fibonacci exercise.

--- javascript-excercises-mysolutions/javascript.js
+++ javascript-excercises-mysolutions/javascript.js
@@ -161,3 +161,17 @@ const palindromes = function (string) {
 
 // Do not edit below this line
 module.exports = palindromes;
+
+const fibonacci = function(value) {
+  let num = Number(value)
+  if (num < 0) {
+    return 'OOPS';
+  }
+  if (num == 1 || num == 0) {
+    return num;
+  }
+  return fibonacci(num - 1) + fibonacci(num - 2);
+};
+
+// Do not edit below this line
+module.exports = fibonacci;
