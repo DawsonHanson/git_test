e599b599aba41bda6ec60b7340e5811633efb2bd
Dawson Hanson <Dawson.Hanson2@gmail.com>
2022-08-22 18:25:33 -0600

    Edit javascript.js File
    Added getTheTitles exercise.

--- javascript-excercises-mysolutions/javascript.js
+++ javascript-excercises-mysolutions/javascript.js
@@ -175,3 +175,13 @@ const fibonacci = function(value) {
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+const getTheTitles = function(array) {
+  const titles = array.map((obj) => {
+    return `${obj.title}`
+  })
+  return titles;
+};
+
+// Do not edit below this line
+module.exports = getTheTitles;
